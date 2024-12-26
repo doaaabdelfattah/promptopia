0206ade0496df3e0e938a99fedd574ef311a7251
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-12-26 10:36:21 +0200

    fix suspense error

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { Suspense, useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "../../components/Form";
 
